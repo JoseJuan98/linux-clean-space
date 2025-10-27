98a5f2e2b7c2e82f5aff404a43d6d4f08960966e
josejuan98 <josejuan-98@hotmail.com>
2025-10-27 23:19:49 +0100

    added minor change

--- src/dnf.sh
+++ src/dnf.sh
@@ -6,7 +6,9 @@ echo -e "\nSize of cache: $(sudo du -sh /var/cache/dnf)\n"
 
 # Remove old versions of kernel
 sudo dnf remove -y $(dnf repoquery --installonly --latest-limit=-2 -q)
+
 # When upgrading to a new version of Fedora, a cache is created. In theory, the cache is cleaned after the upgrade. If not, cleaning can be forced using the following command:
+sudo dnf autoremove -y
 sudo dnf system-upgrade clean
 sudo dnf clean packages
 
